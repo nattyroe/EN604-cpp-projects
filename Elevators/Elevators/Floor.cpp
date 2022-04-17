dfa92b42f6e501cd5bbf73366da6dc15f38de21f
Nathan Roe <nateroe63@gmail.com>
2022-04-17 11:21:41 -0400

    Add docstrings to Floors

--- Elevators/Elevators/Floor.cpp
+++ Elevators/Elevators/Floor.cpp
@@ -3,43 +3,52 @@
 #include "Elevator.h"
 #include "Passenger.h" 
 
+/// Floor destructor
 Floor::~Floor()
 {
+    // Delete future passengers
     while (!this->futurePassengers.empty())
     {
         delete this->futurePassengers.top();
         this->futurePassengers.pop();
     }
+    // Delete waiting up passengers
     while (!this->waitingUpPassengers.empty())
     {
         delete this->waitingUpPassengers.front();
         this->waitingUpPassengers.pop();
     }
+    // Delete waiting down passengers
     while (!this->waitingDownPassengers.empty())
     {
         delete this->waitingDownPassengers.front();
         this->waitingDownPassengers.pop();
     }
-}
+} // End floor destructor
 
+/// Adds provided passenger to the floor
 void Floor::addPassenger(Passenger *passenger)
 {
     this->futurePassengers.push(passenger);
-};
+} // End function addPassenger
 
+/// Notify Floor that sim time has upticked 1 second
 void Floor::incrementTime()
 {
     shiftPassengers();
-}
+} // End function incrementTime
 
+/// Moves future passengers to either Up queue or Down queue
 void Floor::shiftPassengers()
 {
     while (!this->futurePassengers.empty())
     {
+        // Check to see whether any passengers arrive at this second
         if (this->futurePassengers.top()->getStartTime() > this->building->getSimTime())
         {
             break;
         }
+        // If Passenger arrives, move to appropriate queue (up/down)
         else
         {
             Passenger *p = this->futurePassengers.top();
@@ -60,19 +69,18 @@ void Floor::shiftPassengers()
             this->futurePassengers.pop();
         }
     }
-}
+} // End function shiftPassengers
 
-int Floor::getNumFuturePassengers()
-{
-    return this->futurePassengers.size();
-}
-
+/// Get the total number of passengers waiting at the floor
+/// Returns an pair - Passengers Waiting to go up,
+///                   Passengers Waiting to go down
 pair<int, int> Floor::getNumPassengersAtFloor()
 {
     return make_pair(this->getUpPassengerCount(),
                      this->getDownPassengerCount());
-}
+} // End function getNumPassengersAtFloor
 
+/// Adds up to the provided number of UP passengers to the elevator
 void Floor::giveUpPassengers(Elevator *elevator, int maxPassengers)
 {
     int passCount = 0;
@@ -88,14 +96,15 @@ void Floor::giveUpPassengers(Elevator *elevator, int maxPassengers)
     {
         this->elevatorCalled = false;
     }
-}
+} // End function giveUpPassengers
 
+/// Adds up to the provided number of DOWN passengers to the elevator
 void Floor::giveDownPassengers(Elevator *elevator, int maxPassengers)
 {
     int passCount = 0;
     while (passCount < maxPassengers && !this->waitingDownPassengers.empty())
     {
-        Passenger* p = this->waitingDownPassengers.front();
+        Passenger *p = this->waitingDownPassengers.front();
         p->startTravel(this->building->getSimTime());
         elevator->receivePassenger(p);
         this->waitingDownPassengers.pop();
@@ -105,4 +114,4 @@ void Floor::giveDownPassengers(Elevator *elevator, int maxPassengers)
     {
         this->elevatorCalled = false;
     }
-}
+} // End function giveDownPassengers
--- Elevators/Elevators/Floor.h
+++ Elevators/Elevators/Floor.h
@@ -1,3 +1,5 @@
+/// @file
+/// @author Nathan Roe
 #pragma once
 #include <queue>
 #include <utility>
@@ -8,22 +10,67 @@ class Passenger;
 
 using namespace std;
 
+/// Object to represent a Floor in a building
+/// 
+/// Tracks future and currently waiting passengers,
+/// and provides status on need for an elevator
 class Floor
 {
 public:
+    /// Floor constructor
     Floor(Building *building) { this->building = building; };
+    /// Floor destructor
     ~Floor();
 
+    /// Adds provided passenger to the floor
+    /// 
+    /// Adds future passenger, to be shifted to waiting
+    /// at the passenger's StartTime
     void addPassenger(Passenger *passenger);
+
+    /// Notify Floor that sim time has upticked 1 second
+    ///
+    /// Checks to see if any future passengers arrive to
+    /// wait for elevators
     void incrementTime();
-    int getNumFuturePassengers();
-    void resetCallButton() { this->elevatorCalled = false; };
-    bool isElevatorCalled() { return this->elevatorCalled; };
+
+    /// Get number of future passengers
+    /// Returns int of futrure passenger count
+    int getNumFuturePassengers() { return this->futurePassengers.size(); };
+
+    /// Get number of passengers waiting to go up
+    /// Returns int of waitingUpPassenger count
     int getUpPassengerCount() { return this->waitingUpPassengers.size(); };
+
+    /// Get number of passengers waiting to go up
+    /// Returns int of waitingUpPassenger count
     int getDownPassengerCount() { return this->waitingDownPassengers.size(); };
+
+    /// Checks whether elevator is needed at this floor
+    /// Returns TRUE if elevator is needed, FALSE otherwise
+    bool isElevatorCalled() { return this->elevatorCalled; };
+
+    /// Adds up to the provided number of passengers to the elevator
+    /// 
+    /// Shifts passengers waiting to go UP onto the elevator. Number
+    /// of passengers shifted will be the maxPassengers asked for,
+    /// or all up-waiting passengers if less than the cap
+    /// @param *elevator - pointer to the Elevator picking up passengers
+    /// @param maxPassengers - maximum allowed passengers to get on Elevator
     void giveUpPassengers(Elevator *elevator, int maxPassengers);
+
+    /// Adds up to the provided number of passengers to the elevator
+    /// 
+    /// Shifts passengers waiting to go DOWN onto the elevator. Number
+    /// of passengers shifted will be the maxPassengers asked for,
+    /// or all down-waiting passengers if less than the cap
+    /// @param *elevator - pointer to the Elevator picking up passengers
+    /// @param maxPassengers - maximum allowed passengers to get on Elevator
     void giveDownPassengers(Elevator *elevator, int maxPassengers);
 
+    /// Get the total number of passengers waiting at the floor
+    /// Returns an pair - Passengers Waiting to go up,
+    ///                   Passengers Waiting to go down
     pair<int, int> getNumPassengersAtFloor();
 
 private:
@@ -35,5 +82,7 @@ private:
     queue<Passenger*> waitingUpPassengers;
     queue<Passenger*> waitingDownPassengers;
 
+    /// Moves future passengers to either Up queue or Down queue
+    /// if it is their arrival time
     void shiftPassengers();
 };
